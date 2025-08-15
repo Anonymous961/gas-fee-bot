afb900db23034d57e5d1a368e0d369450dd1ce18
Anil Kumar Behera <ark845612@gmail.com>
2025-08-16 02:43:10 +0530

    env fix

--- config/api_keys.py
+++ config/api_keys.py
@@ -1,3 +1,13 @@
 from dotenv import dotenv_values
+import os
 
-config = dotenv_values(".env")
+# Load configuration with priority:
+# 1. Existing environment variables (works in Render)
+# 2. .env file (works locally)
+config = {
+    **dotenv_values(".env"),  # will be empty if .env doesn't exist
+    **os.environ             # will contain Render's environment variables
+}
+
+# Alternative simpler version if you don't need .env support:
+# config = os.environ  # Just use this if you only use environment variables
